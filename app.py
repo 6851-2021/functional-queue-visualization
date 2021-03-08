20ec35dad2d7cc45a40bc18baf9e32e93d45e9eb
Shana Mathew <shanamathew135@gmail.com>
2021-03-07 20:12:49 -0500

    basic backend with simple html frontend

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from .stack import Stack, Node
+
+app = Flask(__name__)
+back_stack = Stack()
+front_stack = Stack()
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+@app.route('/insert', methods = ['POST'])
+def insert_last():
+    if request.method == 'POST':
+        result = request.form
+        print('insert', result)
+        back_stack.push(result["value"])
+        print(back_stack.print_stack())
+
+    return render_template("index.html")
+
+@app.route('/delete', methods = ['POST'])
+def delete_first():
+    if request.method == 'POST':
+        front_stack.pop()
+        print(front_stack.print_stack())
+        print('delete-first!')
+    return render_template("index.html")
---
+++ stack.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+    def set_next(self, next_node):
+        self.next = next_node
+
+class Stack:
+    def __init__(self, head=None):
+        self.head = head
+
+    def set_head(self, head):
+        self.head = head
+    
+    def push(self, val):
+        new_node = Node(val, self.head)
+        self.set_head(new_node)
+    
+    def pop(self):
+        if self.head is None or self.head.next is None:
+            self.head = None
+        else:
+            self.head = self.head.next
+
+    def print_stack(self):
+        node = self.head
+        printed = ""
+        while node is not None:
+            printed += str(node.val) + '->'
+            node = node.next
+        return printed
+
+    def reversed_copy(self):
+        node = self.head
+        reversed_stack = Stack()
+        while node is not None:
+            reversed_stack.push(node.val)
+            node = node.next
+        return reversed_stack
